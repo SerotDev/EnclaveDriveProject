274ee78098d3478c39b4da51f789bfe80a8d6648
Sergi Rodriguez Utge <sergidpm@hotmail.com>
2025-01-21 22:38:07 +0100

    Starting implementation of the color theme managment

--- EnclaveDrive/obj/Debug/net8.0-windows/EnclaveDrive.AssemblyInfo.cs
+++ EnclaveDrive/obj/Debug/net8.0-windows/EnclaveDrive.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnclaveDrive")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38ef194751fdcaec1864a3bfa1d3c1f62221a2f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85a9949dc1e894a0cf1603209d71d90bd898029d")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnclaveDrive")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnclaveDrive")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ EnclaveDrive/utils/ThemeManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace EnclaveDrive.Utils
+{
+    internal class ThemeManager
+    {
+        public class Theme
+        {
+            public string PrimaryColor { get; set; }
+            public string SecondaryColor { get; set; }
+            public string BackgroundColor { get; set; }
+            public string PanelColor { get; set; }
+            public string TextColor { get; set; }
+            public string BorderColor { get; set; }
+        }
+
+        private Dictionary<string, Dictionary<string, Theme>> themes;
+
+        public ThemeManager()
+        {
+            LoadThemes();
+        }
+
+        public void LoadThemes()
+        {
+            string themeFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "Themes.json");
+
+            if (File.Exists(themeFilePath))
+            {
+                var json = File.ReadAllText(themeFilePath);
+                themes = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, Theme>>>(json);
+            }
+            else
+            {
+                throw new FileNotFoundException("El archivo Themes.json no fue encontrado.");
+            }
+        }
+
+        public Theme GetTheme(string mode, string variant)
+        {
+            if (themes.ContainsKey(mode) && themes[mode].ContainsKey(variant))
+            {
+                return themes[mode][variant];
+            }
+
+            throw new ArgumentException($"El tema '{mode}/{variant}' no existe.");
+        }
+    }
+}
